b615e9badb50edbad80b4bf587bda1b2952d57be
Jeremy Black <jb@yodacom.com>
2017-04-28 06:10:27 -0600

    added  http basic authentication

--- index.js
+++ index.js
@@ -2,95 +2,125 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var User = require("./user-model");
+var bcrypt = require("bcryptjs");
+var passport = require("passport");
+var BasicStrategy = require("passport-http").BasicStrategy;
 
 var app = express();
-
 var jsonParser = bodyParser.json();
-var bcrypt = require("bcryptjs");
 
-
-app.post("/users", jsonParser, function (req, res) {
-	if (!req.body) {
-		return res.status(400).json({
-			message: "No request body"
-		});
-	}
-	if (!("username" in req.body)) {
-		return res.status(422).json({
-			message: "Missing field: username"
-		});
-	}
-
-	var username = req.body.username;
-
-	if (typeof username !== "string") {
-		return res.status(433).json({
-			message: "Incorrect field type: username"
-		});
-	}
-
-	username = username.trim();
-
-	if (username === "") {
-		return res.status(422).json({
-			message: "Incorrect field length: username"
-		});
-	}
-
-	if (!("password" in req.body)) {
-		return res.status(433).json({
-			message: "Missing field: password"
-		});
-	}
-
-	var password = req.body.password;
-
-	if (typeof password !== "string") {
-		return res.status(422).json({
-			message: "Incorrect field type: password"
-		});
-	}
-
-	password = password.trim();
-
-	if (password === "") {
-		return res.status(422).json({
-			message: "Incorrect field length: password"
-		});
-	}
-
-	bcrypt.genSalt(10, function (err, salt) {
-		if (err) {
-			return res.status(500).json({
-				message: "Internal server error"
+var strategy = new BasicStrategy(function(username, password, callback){
+	User.findOne({
+		username: username
+	}, function(err, user){
+		if(err) {
+			callback(err);
+			return;
+		}
+		if (!user) {
+			return callback(null, false,{
+				message: "Incorrect username."
 			});
 		}
 
-		bcrypt.hash(password, salt, function (err, hash) {
+		user.validatePassword(password, function(err, isValid) {
+			if(err) {
+				return callback(err);
+			}
+
+			if(!isValid) {
+				return callback (null, false, {
+					message: "incorrect password."
+				});
+			}
+			return callback(null, user);
+		});
+	});
+
+
+
+
+	app.post("/users", jsonParser, function (req, res) {
+		if (!req.body) {
+			return res.status(400).json({
+				message: "No request body"
+			});
+		}
+		if (!("username" in req.body)) {
+			return res.status(422).json({
+				message: "Missing field: username"
+			});
+		}
+
+		var username = req.body.username;
+
+		if (typeof username !== "string") {
+			return res.status(433).json({
+				message: "Incorrect field type: username"
+			});
+		}
+
+		username = username.trim();
+
+		if (username === "") {
+			return res.status(422).json({
+				message: "Incorrect field length: username"
+			});
+		}
+
+		if (!("password" in req.body)) {
+			return res.status(433).json({
+				message: "Missing field: password"
+			});
+		}
+
+		var password = req.body.password;
+
+		if (typeof password !== "string") {
+			return res.status(422).json({
+				message: "Incorrect field type: password"
+			});
+		}
+
+		password = password.trim();
+
+		if (password === "") {
+			return res.status(422).json({
+				message: "Incorrect field length: password"
+			});
+		}
+
+		bcrypt.genSalt(10, function (err, salt) {
 			if (err) {
 				return res.status(500).json({
 					message: "Internal server error"
 				});
 			}
-			var user = new User({
-				username: username,
-				password: hash
-			});
 
-			user.save(function (err) {
+			bcrypt.hash(password, salt, function (err, hash) {
 				if (err) {
 					return res.status(500).json({
 						message: "Internal server error"
 					});
 				}
+				var user = new User({
+					username: username,
+					password: hash
+				});
 
-				return res.status(201).json({});
+				user.save(function (err) {
+					if (err) {
+						return res.status(500).json({
+							message: "Internal server error"
+						});
+					}
+					return res.status(201).json({});
+				});
 			});
 		});
 	});
-});
 
-mongoose.connect("mongodb://localhost/auth").then(function () {
-	app.listen(process.env.PORT || 8080);
-
+	mongoose.connect("mongodb://localhost/auth").then(function() {
+		app.listen(process.env.PORT || 8080);
+	});
 });
